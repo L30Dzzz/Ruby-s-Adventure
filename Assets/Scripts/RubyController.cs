d1c36e3c80b6c819f815cfcbd09705247137c89c
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2022-12-07 13:19:37 -0500

    finished sprites

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -26,17 +26,29 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
         
+        currentHealth = maxHealth;
     }
-
-    
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+        
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -44,7 +56,6 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
     }
-
 
     void FixedUpdate()
     {   
@@ -55,21 +66,21 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
     
-    
     public void ChangeHealth(int amount)
     {
-         if (amount < 0)
+        if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
             
             isInvincible = true;
             invincibleTimer = timeInvincible;
         }
-
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-
 }
+
    

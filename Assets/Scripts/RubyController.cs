2ef6b9dd04cb67bfacf62c7d95700cf01646a357
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2023-02-07 08:48:05 -0500

    finished pt 13

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -88,14 +88,16 @@ public class RubyController : MonoBehaviour
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
-
+    
+    
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
+        projectile.Launch(lookDirection, 500);
 
         animator.SetTrigger("Launch");
     }

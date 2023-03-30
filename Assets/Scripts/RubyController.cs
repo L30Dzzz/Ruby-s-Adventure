00cb1270ed264f9928b279184ff39aca0a815ea9
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2023-03-30 09:14:52 -0400

    dialog

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -172,7 +172,7 @@ public class RubyController : MonoBehaviour
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 350);
+        projectile.Launch(lookDirection, 450);
 
         animator.SetTrigger("Launch");
 
